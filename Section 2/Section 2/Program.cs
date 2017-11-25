6382e6f14b7930ecc9ba5af027bdfc7c7caf853d
Edward Dupre <edwd42@gmail.com>
2017-11-25 17:47:24 -0500

    initial commit Exercise 1 Section 2 Lecture 15

--- Section 2/Section 2/Program.cs
+++ Section 2/Section 2/Program.cs
@@ -1,8 +1,18 @@
-﻿
+﻿using System;
 
-// Section 2 Lecture 13
+/*
+Exercises Section 2 Lecture 15
 
-using System;
+Exercise 1: Design a Stopwatch
+Design a class called Stopwatch. The job of this class is to simulate a stopwatch. It should
+provide two methods: Start and Stop. We call the start method first, and the stop method next.
+Then we ask the stopwatch about the duration between start and stop. Duration should be a
+value in TimeSpan. Display the duration on the console.
+We should also be able to use a stopwatch multiple times. So we may start and stop it and then
+start and stop it again. Make sure the duration value each time is calculated properly.
+We should not be able to start a stopwatch twice in a row (because that may overwrite the initial
+start time). So the class should throw an InvalidOperationException if its started twice. 
+*/
 
 namespace Classes
 {
@@ -11,9 +21,9 @@ namespace Classes
 
         static void Main(string[] args)
         {
-            var cookie = new HttpCookie();
-            cookie["name"] = "Ed";
-            Console.WriteLine(cookie["name"]);
+            var  stopwatch = new Stopwatch();
+            Console.WriteLine("Duration: " + stopwatch.duration);
+
         }
 
     }
---
+++ Section 2/Section 2/Stopwatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Stopwatch
+    {
+        private TimeSpan duration;
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stopwatch()
+        {
+            
+        }
+        
+    }
+}
