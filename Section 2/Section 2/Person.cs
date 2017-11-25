d76636c5c12260fc6433b6d607d997cccb11296c
Edward Dupre <edwd42@gmail.com>
2017-11-25 13:20:54 -0500

    Intermediate Section 2 Lecture 12

---
+++ Section 2/Section 2/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Section 2 Lecture 12
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        // Constructor
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        // Calculated properties
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
--- Section 2/Section 2/Program.cs
+++ Section 2/Section 2/Program.cs
@@ -1,32 +1,16 @@
 ﻿using System;
 
-// Section 2 Lecture 11
+// Section 2 Lecture 12
 
 namespace Classes
 {
-    public class Person
-    {
-        private DateTime _birthdate;
-
-        public void SetBirthdate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
-
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
-    }
-
     class Program
     {
 
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.SetBirthdate(new DateTime(1982, 1, 1));
-            Console.WriteLine(person.GetBirthdate());
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
         }
 
     }
