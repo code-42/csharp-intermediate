2e2ac88863737020acfcb70ac1f7b168db781556
Edward Dupre <edwd42@gmail.com>
2017-11-27 11:10:48 -0500

    Lecture 8 Object Initializers

---
+++ Section 2/ObjectInitializers/ObjectInitializers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                FirstName = "Ed",
+                LastName = "D"
+            };
+
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
