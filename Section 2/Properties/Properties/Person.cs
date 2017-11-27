997d3b7f4f439407b4bc260f26f639b9d3d0d20d
Edward Dupre <edwd42@gmail.com>
2017-11-27 13:54:45 -0500

    completed Lecture 12 - Properties

---
+++ Section 2/Properties/Properties/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        // Shortcut to declare properties - prop, tab code snippet
+        // Convention to put properties at top
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        // Constructor after properties
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        // Calculated properties after constructor
+        public int Age
+        {
+            // No set() accessor method because Birthdate is set in the constructor
+            // because you don't normally need to change someone's birthdate
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ Section 2/Properties/Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
