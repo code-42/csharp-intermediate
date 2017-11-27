1cb98d0ea21cd83240e5aef16751f1ef4c9a9154
Edward Dupre <edwd42@gmail.com>
2017-11-27 14:02:27 -0500

    complete Lecture 13 - Indexers

---
+++ Section 2/Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        // Indexer is an easy way to access elements in Dictionary
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // Indexer
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
---
+++ Section 2/Indexers/Indexers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mosh called this consumer code
+            var cookie = new HttpCookie();
+            cookie["name"] = "Ed";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
