c2f2b88d80bd2004eb4b579c65a30a867b166fc9
Edward Dupre <edwd42@gmail.com>
2017-11-27 13:11:07 -0500

    completed Lecture 9 Methods, try/catch, TryParse() and throw ArgumentNullException

---
+++ Section 2/Methods/Methods/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Point
+    {
+        // Declare fields
+        public int X;
+        public int Y;
+
+        // Constructor
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        // Method
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            // Throw excption if newLocation is null
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+            
+            // Overloaded Move() method calls the other Move() method
+            Move(newLocation.X, newLocation.Y);
+        }
+    }
+}
---
+++ Section 2/Methods/Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+
+
+            int number;
+            var result = int.TryParse("123", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed.");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                //point.Move(new Point(40, 60));
+                point.Move(null);
+                Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+
+                point.Move(new Point(100, 200));
+                Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
+
+        }
+    }
+}
