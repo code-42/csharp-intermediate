fcd8b15711a0f6d54c5a320fd9d13623f642b1b4
Edward Dupre <edwd42@gmail.com>
2017-11-27 13:23:58 -0500

    completed Lecture 10 Fields

---
+++ Section 2/Fields/Fields/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        
+        // Readonly modifier prevents Orders list from being reinitialized
+        public readonly List<Order> Orders = new List<Order>();
+
+        // Constructors
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        // Overloaded constructor
+        public Customer(int id, string name)
+            : this(id) // Calls default ctor
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            //Orders = new List<Order>();            
+            // cannot reinitialize Orders because of readonly keyword
+        }
+    }
+}
---
+++ Section 2/Fields/Fields/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+}
