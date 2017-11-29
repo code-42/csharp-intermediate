57d7518bdb779be2899eb919f9f1373716b234ca
Edward Dupre <edwd42@gmail.com>
2017-11-29 13:25:46 -0500

    Complete Lecture 24 - Boxing and Unboxing

---
+++ Section 4/Lecture 24 - Boxing/Lecture 24 - Boxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_24___Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);  // boxing because 1 is a value
+            list.Add("My Name"); // no boxing
+            list.Add(DateTime.Today); // boxing
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add(null);
+        }
+    }
+}
