c6a274b18e13274d319422842d2705a5c051217b
Edward Dupre <edwd42@gmail.com>
2017-11-29 13:16:07 -0500

    Complete Section 4 Lecture 23 - Upcasting and Downcasting

--- Section 4/Lecture 23 - Casting/Lecture 23 - Casting/Program.cs
+++ Section 4/Lecture 23 - Casting/Lecture 23 - Casting/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,27 @@ namespace Lecture_23___Casting
 
             Console.WriteLine(text.Width); // 100
             Console.WriteLine(shape.Width); // 100
+
+            // Upcasting examples
+            //StreamReader reader = new StreamReader(new FileStream());
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("a string");
+            list.Add(new Text());
+
+            var anotherList = new List<int>();
+            var anotherrList = new List<Shape>();
+
+            // Downcasting
+            // at runtime newShape type will be Text
+            Shape newShape = new Text();
+            // convert (cast) shape to Text
+            Text text2 = (Text) newShape;
+            // now we have access to every shape property
+            text2.FontName = "Courier";
+            Console.WriteLine(text2.FontName);
+
+
         }
     }
 }
