b1666fd088d4ca7059dec5578f088d5c7de03807
Edward Dupre <edwd42@gmail.com>
2017-12-07 14:10:39 -0500

    clean up commented out code

--- Exercises and Assignments/Database Connection - Section 5 Exercise 1 and 2/Database Connection/DbConnection.cs
+++ Exercises and Assignments/Database Connection - Section 5 Exercise 1 and 2/Database Connection/DbConnection.cs
@@ -3,31 +3,15 @@ using System.Collections.Generic;
 
 namespace Database_Connection
 {
-    //partial class Program
-    //{
     // Base class
     public abstract class DbConnection
         {
             // base class called DbConnection. This class should have two properties:
             public string _connectionString { get; set; }
             private TimeSpan _timeout { get; set; }
-
-        public abstract void OpenConnection(string SQLconnectionString, TimeSpan Timeout); // { Console.WriteLine("Default open implementation\n"); }
-        public abstract void CloseConnection(); // { Console.WriteLine("Default close implementation\n"); }
+            
+            // Declare abstract methods for abstract class
+            public abstract void OpenConnection(string SQLconnectionString, TimeSpan Timeout);
+            public abstract void CloseConnection();
         }
-
-        //public class Connection
-        //{
-        //    string SQLconnectionString;
-        //    TimeSpan Timeout;
-
-        //    public void Connect(List<DbConnection> connections)
-        //    {
-        //        foreach (var connection in connections)
-        //        {
-        //            connection.OpenConnection(SQLconnectionString, Timeout);
-        //        }
-        //    }
-        //}
-    //}
 }
